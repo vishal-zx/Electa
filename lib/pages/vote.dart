e81f835d7ca89bf84641e3a824013f8916f6c4c3
Vishal Gupta <19ucs053@lnmiit.ac.in>
2021-06-24 18:42:16 +0530

    24june 6.42 pm

--- lib/pages/vote.dart
+++ lib/pages/vote.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:electa/widgets/drawer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Vote extends StatefulWidget {
@@ -75,12 +76,66 @@ class _VoteState extends State<Vote> {
                                   ),
                                   Column(
                                     children: [
-                                      Container(
-                                        child: Text("Hello"),
-                                        padding: EdgeInsets.all(25),  
+                                      SizedBox(
+                                        height: MediaQuery.of(context).size.height*0.03,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        // crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            width: MediaQuery.of(context).size.height*0.10,
+                                            height: MediaQuery.of(context).size.height*0.10,
+                                            decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                image: NetworkImage("https://www.mantruckandbus.com/fileadmin/media/bilder/02_19/219_05_busbusiness_interviewHeader_1485x1254.jpg"),
+                                                fit: BoxFit.fill
+                                              ),
+                                              ),
+                                          ),
+                                          SizedBox(
+                                            width: MediaQuery.of(context).size.width*0.3,
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text("Vishal Gupta",
+                                                  style: TextStyle(
+                                                    fontSize: 20,
+                                                    color: Colors.white,
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  height: 2,
+                                                ),
+                                                Text("19UCS053",
+                                                  style: TextStyle(
+                                                    fontSize: 15,
+                                                    color: Colors.white,
+                                                  ),                                                
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: MediaQuery.of(context).size.width*0.1,
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              children: [
+                                                Icon(Icons.beenhere_outlined,
+                                                  size: 35,
+                                                  color: Colors.white,
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        height: MediaQuery.of(context).size.height*0.05,
                                       ),
                                     ],
-                                  )
+                                  ),
                                 ],
                               ),
                             ),
