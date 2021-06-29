1d69f2cdda8ef2379a58b5579fb3263948ad1a35
manya <09.manyasharma@gmail.com>
2021-06-29 12:58:59 +0530

    result.dart added

---
+++ lib/pages/result.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+class resultscreen extends StatefulWidget {
+  @override
+  _resultscreenState createState() => _resultscreenState();
+}
+
+// ignore: camel_case_types
+class _resultscreenState extends State<resultscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: new Text("Results"),
+      ),
+        backgroundColor: Colors.blueGrey,
+        body: new Center(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                    width: 190.0,
+                    height: 190.0,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: new NetworkImage(
+                                "https://image.freepik.com/free-vector/candidate-election-campaign_74855-6281.jpg")
+                        )
+                    )
+                ),
+                new Text("John De",
+                    textScaleFactor: 1.5,
+                style: TextStyle(fontWeight: FontWeight.bold, height: 1.7),
+                ),
+
+              ],
+            ),
+        ),
+    );
+
+
+  }
+}
+
