ab590337de369b54289cb3d7cff3507a264661df
manya <09.manyasharma@gmail.com>
2021-07-01 02:38:10 +0530

    slimy card in result.dart

--- lib/pages/result.dart
+++ lib/pages/result.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:slimy_card/slimy_card.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 
 class resultscreen extends StatefulWidget {
@@ -11,11 +13,120 @@ class _resultscreenState extends State<resultscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[300],
+        automaticallyImplyLeading: false,
+        title: Text("Results"),
+      ),
+      body: StreamBuilder(
+        initialData: false,
+        stream: slimyCard.stream,
+        builder: ((BuildContext context, AsyncSnapshot snapshot) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SizedBox(height: 70),
+              SlimyCard(
+                color: Colors.indigo[300],
+                topCardWidget: topCardWidget(),
+                bottomCardWidget: bottomCardWidget(),
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget topCardWidget() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(image: AssetImage('images/cc.jpg')),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 20,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'President \nSaumitra Vyas',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 15),
+        Center(
+          child: Text(
+            "Congratulations, Saumitra Vyas! for winning this year's presidential election."
+            "All the best for your new position.",
+            style: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+                fontSize: 14,
+                fontWeight: FontWeight.w500),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+  Widget bottomCardWidget() {
+    
+    return Column(
+      children: [
+        Text(
+          'Saumitra Vyas V/S Manya Sharma',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+          textAlign: TextAlign.center,
+        ),
+
+
+
+
+        SizedBox(height: 15),
+        Expanded(
+          child: Text(
+            'FlutterDevs specializes in creating cost-effective and efficient '
+                'applications with our perfectly crafted,creative and leading-edge '
+                'flutter app development solutions for customers all around the globe.',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+/*return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         title: new Text("Results"),
       ),
         backgroundColor: Colors.blueGrey,
+
+
+
         body: new Center(
             child: new Column(
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -33,17 +144,21 @@ class _resultscreenState extends State<resultscreen> {
                         )
                     )
                 ),
-                new Text("John De",
+                new Text("John De \nIII Year \nCSE",
                     textScaleFactor: 1.5,
-                style: TextStyle(fontWeight: FontWeight.bold, height: 1.7),
+                style: TextStyle(fontWeight: FontWeight.normal, height: 1.7),
+                  textAlign: TextAlign.center,
+                ),
+                new Text("Congratulations, John!",
+                  textScaleFactor: 1.5,
+                  style: TextStyle(fontWeight: FontWeight.normal, height: 1.7, color: Colors.blue,),
+                  textAlign: TextAlign.center,
                 ),
 
               ],
             ),
         ),
-    );
+    );*/
 
 
-  }
-}
 
