c48f1e45e50c54c3506cb3667408e39d1e71b953
Saumitra <19ucs252@lnmiit.ac.in>
2021-07-05 11:21:20 +0530

    checking

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -31,15 +31,6 @@ class _RegisterState extends State<Register> {
       });
   }
 
-  bool isInitialized = false;
-  @override
-  void initState() {
-    FlutterMobileVision.start().then((value) {
-      isInitialized = true;
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     var elevatedbutton = ElevatedButton(
@@ -48,7 +39,7 @@ class _RegisterState extends State<Register> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "ELECTA",
+          "Register",
           textDirection: TextDirection.ltr,
         ),
         elevation: 0,
@@ -60,25 +51,33 @@ class _RegisterState extends State<Register> {
       ),
       body: Column(
         children: <Widget>[
+          SizedBox(
+            height: 20.0,
+          ),
+          Image.asset(
+            "assets/images/sign up.png",
+            fit: BoxFit.fitHeight,
+            // height: 400,
+          ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(10, 8, 40, 0),
+            padding: EdgeInsets.fromLTRB(10, 4, 40, 0),
             child: TextField(
               decoration: InputDecoration(
                 icon: Icon(
                   Icons.person_pin,
-                  color: Colors.red,
+                  color: Colors.lightBlue,
                 ),
                 hintText: "Enter Your Roll Number",
               ),
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(10, 8, 40, 0),
+            padding: EdgeInsets.fromLTRB(10, 3, 40, 0),
             child: TextField(
               decoration: InputDecoration(
                 icon: Icon(
                   Icons.person_outline_sharp,
-                  color: Colors.red,
+                  color: Colors.lightBlue,
                 ),
                 hintText: "Enter Your Name",
               ),
@@ -87,115 +86,109 @@ class _RegisterState extends State<Register> {
           Row(
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.only(top: 16.0, left: 15.0),
+                padding: EdgeInsets.only(top: 8.0, left: 15.0),
                 child: Text("DOB : ",
                     textDirection: TextDirection.ltr,
-                    style: TextStyle(fontSize: 20.0)),
+                    style: TextStyle(fontSize: 15.0)),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 17.0),
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text(selectedDate.day.toString()),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 17.0),
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text("/"),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 17.0),
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text(selectedDate.month.toString()),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 17.0),
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text("/"),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 17.0),
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text(selectedDate.year.toString()),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 18.0, left: 10.0),
+                padding: EdgeInsets.only(top: 12.0, left: 10.0),
                 child: SizedBox(child: elevatedbutton),
               ),
             ],
           ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0, left: 50.0),
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: Text('scan your college id card')),
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.only(left: 25.0),
-              //   child: buildcheckbox(),
-              // ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0, left: 32.0),
-                child: ElevatedButton(
-                    onPressed: () => {},
-                    child: Text('------Face Authentication------')),
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.only(left: 28.0),
-              //   child: buildCheckbox(),
-              // ),
-            ],
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50.0),
-              child: Text(
-                'you want to register yourself as:',
-                style: TextStyle(
-                  fontSize: 22.0,
-                  foreground: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 1
-                    ..color = Colors.black87,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 80.0, top: 32.0),
-                child:
-                    ElevatedButton(onPressed: () => {}, child: Text('VOTER')),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 30.0, top: 32.0),
-                child: ElevatedButton(
-                    onPressed: () => {}, child: Text('CANDIDATE')),
-              ),
-            ],
-          ),
-          // ignore: deprecated_member_use
           Padding(
-            padding: const EdgeInsets.only(top: 20.0),
-            child: ColorizeAnimatedTextKit(
-              text: ['you have succesfully registered as VOTER/CANDIDATE'],
-              colors: [
-                Colors.black,
-                Colors.brown,
-                Colors.purple,
-                Colors.red,
-                Colors.yellow,
-                Colors.orange,
-                Colors.lightBlue
-              ],
-              textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
-              textAlign: TextAlign.center,
-              isRepeatingAnimation: true,
-              speed: Duration(milliseconds: 300),
-              totalRepeatCount: 5,
-            ),
-          )
+            padding: EdgeInsets.only(right: 200.0),
+            child: ElevatedButton(
+                onPressed: () => {}, child: Text('scan your college id card')),
+          ),
+
+          // Padding(
+          //   padding: const EdgeInsets.only(left: 25.0),
+          //   child: buildcheckbox(),
+          // ),
+
+          Padding(
+            padding: EdgeInsets.only(right: 225.0),
+            child: ElevatedButton(
+                onPressed: () => {}, child: Text('Face Authentication')),
+          ),
+
+          // Padding(
+          //   padding: const EdgeInsets.only(left: 28.0),
+          //   child: buildCheckbox(),
+          // ),
+
+          // Center(
+          //   child: Padding(
+          //     padding: const EdgeInsets.only(top: 10.0),
+          //     child: Text(
+          //       'you want to register yourself as:',
+          //       style: TextStyle(
+          //         fontSize: 22.0,
+          //         foreground: Paint()
+          //           ..style = PaintingStyle.stroke
+          //           ..strokeWidth = 1
+          //           ..color = Colors.black87,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // Row(
+          //   children: <Widget>[
+          //     Padding(
+          //       padding: const EdgeInsets.only(left: 80.0, top: 20.0),
+          //       child:
+          //           ElevatedButton(onPressed: () => {}, child: Text('VOTER')),
+          //     ),
+          //     Padding(
+          //       padding: EdgeInsets.only(left: 30.0, top: 20.0),
+          //       child: ElevatedButton(
+          //           onPressed: () => {}, child: Text('CANDIDATE')),
+          //     ),
+          //   ],
+          // ),
+          // // ignore: deprecated_member_use
+          // Padding(
+          //   padding: const EdgeInsets.only(top: 20.0),
+          //   child: ColorizeAnimatedTextKit(
+          //     text: ['you have succesfully registered as VOTER/CANDIDATE'],
+          //     colors: [
+          //       Colors.black,
+          //       Colors.brown,
+          //       Colors.purple,
+          //       Colors.red,
+          //       Colors.yellow,
+          //       Colors.orange,
+          //       Colors.lightBlue
+          //     ],
+          //     textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+          //     textAlign: TextAlign.center,
+          //     isRepeatingAnimation: true,
+          //     speed: Duration(milliseconds: 300),
+          //     totalRepeatCount: 5,
+          //   ),
+          // )
         ],
       ),
     );
