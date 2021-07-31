efbe2aed18fb663c2950d8bfbb4ee3ffa41b53f4
Vishal Gupta <19ucs053@lnmiit.ac.in>
2021-07-31 20:13:36 +0530

    refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,24 @@ import 'pages/home.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+String route = MyRoutes.loginRoute;
+
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('email');
-  print(email);
-  runApp(email==null ? LoginApp() : HomeApp());
+  route = (email==null)? MyRoutes.loginRoute : MyRoutes.homeRoute;
+  runApp(MyApp());
 }
 
-class LoginApp extends StatefulWidget {
-  const LoginApp({ Key? key }) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
-  _LoginAppState createState() => _LoginAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _LoginAppState extends State<LoginApp> {
+class _MyAppState extends State<MyApp> {
 
   bool _initialized = false;
   bool _error = false;
@@ -81,82 +83,7 @@ class _LoginAppState extends State<LoginApp> {
         // primaryTextTheme: GoogleFonts.latoTextTheme()
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.loginRoute,
-      routes: {
-        MyRoutes.loginRoute : (context) => LoginPage(),
-        MyRoutes.registerRoute : (context) => Register(),
-        MyRoutes.homeRoute : (context) => Home(),
-        MyRoutes.voteRoute : (context) => Vote(),
-        MyRoutes.resultRoute : (context) => Result(),
-        MyRoutes.myAccountRoute : (context) => MyAccount(),
-        MyRoutes.changePswdRoute : (context) => ChangePswd(),
-        MyRoutes.hNSRoute : (context) => HelpNSupport(),
-        MyRoutes.editProfileRoute : (context) => EditProfile(),
-        MyRoutes.userProfileRoute : (context) => UserProfile(),
-      },
-    );
-  }
-}
-
-class HomeApp extends StatefulWidget {
-  const HomeApp({ Key? key }) : super(key: key);
-
-  @override
-  _HomeAppState createState() => _HomeAppState();
-}
-
-class _HomeAppState extends State<HomeApp> {
-
-  bool _initialized = false;
-  bool _error = false;
-
-  // Define an async function to initialize FlutterFire
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-      });
-    } catch(e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _error = true;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    initializeFlutterFire();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    if(_error) {
-      return Container();
-    }
-
-    // Show a loader until FlutterFire is initialized
-    if (!_initialized) {
-      return Container();
-    }
-    
-    return MaterialApp(
-      // home: Home(),
-      themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        // primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.homeRoute,
+      initialRoute: route,
       routes: {
         MyRoutes.loginRoute : (context) => LoginPage(),
         MyRoutes.registerRoute : (context) => Register(),
