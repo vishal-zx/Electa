073a247588720b4c2bf528c5355fb77619c9b8b5
Vishal Gupta <19ucs053@lnmiit.ac.in>
2021-08-09 22:45:48 +0530

    confetti widget added ~manya

--- lib/widgets/All_Confetti_Widget.dart
+++ lib/widgets/All_Confetti_Widget.dart
@@ -1,19 +1,23 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:confetti/confetti.dart';
+// ignore: import_of_legacy_library_into_null_safe
+//import 'package:confetti/src/enums/confetti_controller_state.dart';
 
 class AllConfettiWidget extends StatefulWidget {
   final Widget child;
 
   const AllConfettiWidget({
     required this.child,
-    required Key key,
-}) : super(key: key);
+    Key? key,
+  }) : super(key: key);
   @override
   _AllConfettiWidgetState createState() => _AllConfettiWidgetState();
 }
 
 class _AllConfettiWidgetState extends State<AllConfettiWidget> {
-  late ConfettiController controller;
+  ConfettiController controller = ConfettiController(duration: Duration(seconds: 3));
   @override
   void initState() {
     super.initState();
@@ -22,26 +26,54 @@ class _AllConfettiWidgetState extends State<AllConfettiWidget> {
     controller.play();
   }
 
+
+  static final double right = 0;
+  static final double down = pi/2;
+  static final double left = pi;
+  static final double top = -pi/2;
+
+  final double blastDirection = top;
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: ConfettiWidget(
-              confettiController: controller,
-            colors: [
-              Colors.red,
-              Colors.blue,
-              Colors.green,
-              Colors.yellow,
-              Colors.pinkAccent,
-              Colors.purple,
-            ],
-          ),
-        ),
-        widget.child,
-      ],
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        if(controller.state == ConfettiControllerState.playing){
+          controller.stop();
+        } else{
+          controller.play();
+        }
+      },
+      child: Stack(
+        children: [
+          widget.child,
+          buildConfetti(),
+        ],
+      ),
     );
   }
+
+  Widget buildConfetti() => Align(
+    alignment: Alignment.center,
+    child: ConfettiWidget(
+      confettiController: controller,
+      colors: [
+        Colors.red,
+        Colors.blue,
+        Colors.green,
+        Colors.yellow,
+        Colors.pinkAccent,
+        Colors.purple,
+        Colors.deepOrangeAccent,
+      ],
+      // blastDirection: blastDirection,
+      blastDirectionality: BlastDirectionality.explosive,
+      shouldLoop: true,
+      emissionFrequency: 0,
+      numberOfParticles: 25,
+    ),
+  );
+
+
 }
