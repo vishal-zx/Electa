23abb26daaa06e2f2edd736ad81717bcc13a4372
mrjoy1103 <19ucs245@lnmiit.ac.in>
2021-08-19 17:09:57 +0530

    just small func

---
+++ lib/pages/addPost.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class addPost extends StatefulWidget {
+  const addPost({Key? key}) : super(key: key);
+
+  @override
+  _addPostState createState() => _addPostState();
+}
+
+class _addPostState extends State<addPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Post"),
+      ),
+
+      body: Container(alignment: ,),
+    );
+  }
+}
