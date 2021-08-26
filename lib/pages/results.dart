71e21f28fad01f88209a38d17745da23b4751153
Saumitra <19ucs252@lnmiit.ac.in>
2021-08-26 17:32:04 +0530

    result page changes

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -11,70 +11,67 @@ import 'package:pie_chart/pie_chart.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:confetti/confetti.dart';
 
-
-
 class Result extends StatefulWidget {
-  const Result({ key }) : super(key: key);
-
+  const Result({key}) : super(key: key);
 
   @override
   _ResultState createState() => _ResultState();
 }
 
 // ignore: unused_element
-Widget _buildResultRow(BuildContext context, String winner, String roll, String image, String position){
-  return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-  Container(
-  
-    width: MediaQuery.of(context).size.height*0.09,
-    height: MediaQuery.of(context).size.height*0.09,
-    child: ClipRRect(
-      borderRadius: BorderRadius.circular(50),
-      child: CachedNetworkImage(
-        fit: BoxFit.fill,
-        imageUrl: image,
-        progressIndicatorBuilder: (context, url, downloadProgress) => 
-                CircularProgressIndicator(value: downloadProgress.progress),
-        errorWidget: (context, url, error) => Image(image: AssetImage("assets/images/u1.png")),
-      ),
-    ),
-  ),
-  SizedBox(
-    width: MediaQuery.of(context).size.width*0.35,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Text("$winner",
-            style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
+Widget _buildResultRow(BuildContext context, String winner, String roll,
+    String image, String position) {
+  return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+    Container(
+      width: MediaQuery.of(context).size.height * 0.09,
+      height: MediaQuery.of(context).size.height * 0.09,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: image,
+          progressIndicatorBuilder: (context, url, downloadProgress) =>
+              CircularProgressIndicator(value: downloadProgress.progress),
+          errorWidget: (context, url, error) =>
+              Image(image: AssetImage("assets/images/u1.png")),
         ),
       ),
-     SizedBox(
-       height: 5,
-      ),
-      FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Text("$roll",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.white,
+    ),
+    SizedBox(
+      width: MediaQuery.of(context).size.width * 0.35,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              "$winner",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(
+            height: 5,
+          ),
+          FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              "$roll",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     ),
-  ),
-  ]
-  );
+  ]);
 }
 
-class UserCandidate{
+class UserCandidate {
   var name;
   var rollNo;
   var imageUrl;
@@ -82,10 +79,11 @@ class UserCandidate{
   var counter;
   var title;
 
-  UserCandidate(this.name, this.rollNo, this.imageUrl, this.bio, this.counter, this.title);
+  UserCandidate(this.name, this.rollNo, this.imageUrl, this.bio, this.counter,
+      this.title);
 }
 
-class CandidateResult{
+class CandidateResult {
   final winner;
   final rollNo;
   final imageUrl;
@@ -94,23 +92,35 @@ class CandidateResult{
 }
 
 class _ResultState extends State<Result> {
-  int _index = 0, cpos=0;
-  var positions = ["President", "Vice-President", "G-Sec Science", "G-Sec Cultural", "G-Sec Sports", "AG-Sec Science", "AG-Sec Cultural", "AG-Sec Sports",];
+  int _index = 0, cpos = 0;
+  var positions = [
+    "President",
+    "Vice-President",
+    "G-Sec Science",
+    "G-Sec Cultural",
+    "G-Sec Sports",
+    "AG-Sec Science",
+    "AG-Sec Cultural",
+    "AG-Sec Sports",
+  ];
 
   get key => null;
 
   final List<List<UserCandidate>> allCans = [];
 
-  CollectionReference _collectionRef = FirebaseFirestore.instance.collection('candidates');
+  CollectionReference _collectionRef =
+      FirebaseFirestore.instance.collection('candidates');
 
   Future<void> getData() async {
-    for(int i=0;i<positions.length;i++) {
-      QuerySnapshot querySnapshot = await _collectionRef.where('title', isEqualTo: positions[i]).get();
+    for (int i = 0; i < positions.length; i++) {
+      QuerySnapshot querySnapshot =
+          await _collectionRef.where('title', isEqualTo: positions[i]).get();
       var cans = querySnapshot.docs;
-      List<UserCandidate> temp=[];
-      for(var can in cans){
+      List<UserCandidate> temp = [];
+      for (var can in cans) {
         Map<String, dynamic> data = can.data() as Map<String, dynamic>;
-        UserCandidate c = new UserCandidate(data['Name'], data['Roll'], data['imageUrl'], data['Bio'], data['counter'], data['title']);
+        UserCandidate c = new UserCandidate(data['Name'], data['Roll'],
+            data['imageUrl'], data['Bio'], data['counter'], data['title']);
         temp.add(c);
       }
       allCans.add(temp);
@@ -118,15 +128,15 @@ class _ResultState extends State<Result> {
   }
 
   UserCandidate findWinner(List<UserCandidate> cans) {
-    int mx=0, j=-1;
-    for(int i=0;i<cans.length;i++){
-      if(cans[i].counter != "0" && cans[i].counter >= mx){
+    int mx = 0, j = -1;
+    for (int i = 0; i < cans.length; i++) {
+      if (cans[i].counter != "0" && cans[i].counter >= mx) {
         mx = cans[i].counter;
-        j=i;
+        j = i;
       }
     }
-    for(int i=0;i<cans.length;i++){
-      if(cans[i].counter == mx){
+    for (int i = 0; i < cans.length; i++) {
+      if (cans[i].counter == mx) {
         // tie condition
       }
     }
@@ -134,16 +144,15 @@ class _ResultState extends State<Result> {
   }
 
   bool allRight = false;
-  @override 
-  void initState(){
-    if(allCans.length == 0){
-      getData().then((val){
-        setState((){
+  @override
+  void initState() {
+    if (allCans.length == 0) {
+      getData().then((val) {
+        setState(() {
           allRight = true;
         });
       });
-    }
-    else{
+    } else {
       allRight = true;
     }
     super.initState();
@@ -152,104 +161,128 @@ class _ResultState extends State<Result> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       drawer: MyDrawer(),
+      drawer: MyDrawer(),
       appBar: AppBar(
         title: Text("Results"),
         elevation: 10,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: MediaQuery.of(context).size.height * 0.12,
-              child: Center(
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text("Congratulations! to all the selected candidates \nAll the best for your upcoming responsibilities.",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.black,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-              ),
-              ),
-            ),
-            Container(
-              child: (allRight==false)?
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SpinKitCubeGrid(
-                    color: Colors.grey,
-                    size: 50.0,
-                    duration: Duration(seconds: 3),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height*0.01,
-                  ),
-                  Text(
-                    "Loading Data ...",
-                    style: TextStyle(
-                      fontSize: 19,
-                      color: Colors.white
-                    )
-                  ),
-                ],
-              ):SizedBox(
-              height: MediaQuery.of(context).size.height * 0.6,
-              child: Container(
-                
-                child: PageView.builder(
-                  itemCount: positions.length,
-                  controller: PageController(viewportFraction: 0.75),
-                  onPageChanged: (int index) => setState(() => _index = index),
-                  itemBuilder: (_, i) {
-                    return Transform.scale(
-                      scale: i == _index ? 1 : 0.8,
-                      child: StreamBuilder(
-                        initialData: false,
-                        stream: slimyCard.stream,
-                        builder: ((BuildContext context, AsyncSnapshot snapshot)
-                        {
-                          return ListView(
-                            padding: EdgeInsets.zero,
-                            children: <Widget>[
-                              SizedBox(height: 70),
-                              SlimyCard(
-                                topCardWidget: topCardWidget(i),
-                                bottomCardWidget: bottomCardWidget(),
-                              ),
-                            ],
-                          );
-                        }
+          child: (allRight == false)
+              ? Center(
+                  heightFactor: 8.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SpinKitCubeGrid(
+                        color: Colors.grey,
+                        size: 50.0,
+                        duration: Duration(seconds: 3),
                       ),
-                    ),
-                    );
-                  },
-                ),
-              ),
-            )
-            )
-          ]
-        )
-      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.01,
+                      ),
+                      Text("Loading Data ...",
+                          style: TextStyle(fontSize: 19, color: Colors.black)),
+                    ],
+                  ),
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                      SizedBox(height: 30),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.height * 0.10,
+                        child: Center(
+                          child: Text(
+                            "Congratulations! to all the selected candidates \nAll the best for your upcoming responsibilities.",
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Container(
+                          height: MediaQuery.of(context).size.height * 0.7,
+                          // child: (allRight == false)
+                          //     ? Column(
+                          //         mainAxisAlignment: MainAxisAlignment.center,
+                          //         children: [
+                          //           SpinKitCubeGrid(
+                          //             color: Colors.grey,
+                          //             size: 50.0,
+                          //             duration: Duration(seconds: 3),
+                          //           ),
+                          //           SizedBox(
+                          //             height:
+                          //                 MediaQuery.of(context).size.height *
+                          //                     0.01,
+                          //           ),
+                          //           Text("Loading Data ...",
+                          //               style: TextStyle(
+                          //                   fontSize: 19, color: Colors.white)),
+                          //         ],
+                          //       )
+                          //     :
+                          child: SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.7,
+                            child: Container(
+                              child: PageView.builder(
+                                itemCount: positions.length,
+                                controller:
+                                    PageController(viewportFraction: 0.75),
+                                onPageChanged: (int index) =>
+                                    setState(() => _index = index),
+                                itemBuilder: (_, i) {
+                                  return Transform.scale(
+                                    scale: i == _index ? 1 : 0.8,
+                                    child: StreamBuilder(
+                                      initialData: false,
+                                      stream: slimyCard.stream,
+                                      builder: ((BuildContext context,
+                                          AsyncSnapshot snapshot) {
+                                        return ListView(
+                                          padding: EdgeInsets.zero,
+                                          children: <Widget>[
+                                            SizedBox(height: 20),
+                                            SlimyCard(
+                                              color: Colors.blueGrey,
+                                              topCardWidget: topCardWidget(i),
+                                              bottomCardWidget:
+                                                  bottomCardWidget(),
+                                            ),
+                                          ],
+                                        );
+                                      }),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ))
+                    ])),
     );
   }
 
   topCardWidget(int i) {
     return Container(
       decoration: BoxDecoration(
-           gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Color(0xff63717e), Color(0xff4d6277), Color(0xff2d5173), Color(0xff003f6b), Color(0xff003b6d)]
-                ),            
-                 ),
-      padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.01),
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xff63717e),
+              Color(0xff4d6277),
+              Color(0xff2d5173),
+              Color(0xff003f6b),
+              Color(0xff003b6d)
+            ]),
+      ),
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
@@ -266,24 +299,27 @@ class _ResultState extends State<Result> {
             endIndent: 25,
           ),
           SizedBox(height: 15),
-          AllConfettiWidget(child: Center(
-            child: Container(
-          width: MediaQuery.of(context).size.height*0.13,
-          height: MediaQuery.of(context).size.height*0.13,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50),
-            child: CachedNetworkImage(
-              fit: BoxFit.fill,
-              imageUrl: findWinner(allCans[i]).imageUrl,
-              progressIndicatorBuilder: (context, url, downloadProgress) => 
-                      CircularProgressIndicator(value: downloadProgress.progress),
-              errorWidget: (context, url, error) => Image(image: AssetImage("assets/images/u1.png")),
-            ),
-          ),
-        ),
-          ), key: key),
-          
-        
+          AllConfettiWidget(
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.height * 0.13,
+                  height: MediaQuery.of(context).size.height * 0.13,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: CachedNetworkImage(
+                      fit: BoxFit.fill,
+                      imageUrl: findWinner(allCans[i]).imageUrl,
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) =>
+                              CircularProgressIndicator(
+                                  value: downloadProgress.progress),
+                      errorWidget: (context, url, error) =>
+                          Image(image: AssetImage("assets/images/u1.png")),
+                    ),
+                  ),
+                ),
+              ),
+              key: key),
           SizedBox(height: 15),
           Text(
             findWinner(allCans[i]).name,
@@ -303,14 +339,19 @@ class _ResultState extends State<Result> {
   bottomCardWidget() {
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [Color(0xff63717e), Color(0xff4d6277), Color(0xff2d5173), Color(0xff003f6b), Color(0xff003b6d)]
-          ),
-          ),
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xff63717e),
+              Color(0xff4d6277),
+              Color(0xff2d5173),
+              Color(0xff003f6b),
+              Color(0xff003b6d)
+            ]),
+      ),
       child: Column(
-        
         children: [
           Text(
             'Saumitra Vyas V/S Manya Sharma',
@@ -323,7 +364,9 @@ class _ResultState extends State<Result> {
           ),
           SizedBox(height: 15),
           Expanded(
-            child: PieChart(dataMap: dataMap,),
+            child: PieChart(
+              dataMap: dataMap,
+            ),
             /*child: Text(
               'FlutterDevs specializes in creating cost-effective and efficient '
                   'applications with our perfectly crafted,creative and leading-edge '
@@ -335,15 +378,14 @@ class _ResultState extends State<Result> {
               ),
               textAlign: TextAlign.center,
             ),*/
-
           ),
-
         ],
       ),
     );
   }
 }
+
 Map<String, double> dataMap = {
-    "Manya Sharma": 5,
-    "Saumitra Vyas": 2,
-  };
+  "Manya Sharma": 5,
+  "Saumitra Vyas": 2,
+};
